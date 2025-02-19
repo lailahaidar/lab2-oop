d96a962930da2766efee62d16bea949b8b7ade47
lailahaidar <145322841+lailahaidar@users.noreply.github.com>
2025-02-19 12:44:19 +0300

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,19 @@
+#Bank Account Class stores owner and balance #balance always starts at 0
+
+#deposit money to the account
+
+#withdraw money from the account
+
+#general print function owner and balance
+
+#create 3 bank accounts
+
+#create a list to keep information about the accounts
+
+#add 3 objects of type BankAccount to the list
+
+#print the items in the list
+
+#Create a savings account has all the functions af a bank account #plus an interest rate
+
+#print one account with the interest
