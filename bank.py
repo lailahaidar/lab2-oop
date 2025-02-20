3bbc9646ba5817ba151de336f4db70123505637b
Asomalr <asmaalrashidi27@gmail.com>
2025-02-20 11:50:45 +0300

    Asmaa

--- bank.py
+++ bank.py
@@ -17,3 +17,37 @@
 #Create a savings account has all the functions af a bank account #plus an interest rate
 
 #print one account with the interest
+
+class BankAccount:
+    def init (self,owner,balance=0):
+    self.owner = owner
+    self.balance = balance
+    def deposeit(self, amount):
+        self.balance += amount
+    def withdraw(self,amount):
+        if ampunt < self.balance:
+            print("insufficient funds")
+        else:
+            self.b alance -= amount 
+    def str (self):
+        return f"owner: {self.owner},
+        balance: {self,balance}"
+        class SavingsAccount(BankAccount):
+            def init (self, owner, balance=0, interest_rate=0.01):
+                super() init (owner, balance)
+                self.interest_rate =
+                interest_rate
+                def str (self);
+                return f"owner: {self.owner},
+                balance: {self.balance}, interest Rate:
+                {self.interest_rate}"
+account1=BankAccount("Alice")
+account2=BankAccount("Bob",100)
+account3=SavingsAccount("Charlie",200,0.02)
+
+accounts = [account1, account2, account3]
+for account in accounts:
+    print(account)
+print(accounts[2])
+
+            
